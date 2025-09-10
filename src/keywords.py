51218673dbdc202f964f505239bfdab684c7f73b
dreaydemir <ensaraydemir@gmail.com>
2025-09-10 11:54:14 +0300

    keywords.py

---
+++ src/keywords.py
@@ -0,0 +1,7 @@
+KEYWORDS = [
+    "machine learning", "deep learning", "artificial intelligence",
+    "parathyroid", "thyroid cancer", "osteoporosis", "diabetes",
+    "adrenal incidentaloma", "cushing", "pheochromocytoma",
+    "pituitary", "acromegaly", "hypopituitarism", "growth hormone",
+    "bone metabolism", "calcium metabolism", "vitamin D",
+]
